98ec1b1f580ebdee631a512f2ae9b3337fc4554d
Aman Awdhani <Amanawdhani19@gmail.com>
2019-11-06 20:13:14 +0530

    Add files via upload
    Simple Snake game on Unity2d. Use it and give your touch.

---
+++ SnakeGame/Assets/Script/Manager.cs
@@ -0,0 +1,129 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    SnakeMovement instance;
+
+    [Header("gameobjects")]
+    public GameObject tailPart;
+    public GameObject food;
+    GameObject foodClone;
+
+    [Header("List")]
+    List<Vector2> tailPos = new List<Vector2>();
+    List<GameObject> tail = new List<GameObject>();
+
+    float x, y;
+    float gridHeight, gridWidth;
+    int totalEat;
+
+    void Start () {
+
+        instance = FindObjectOfType<SnakeMovement>();
+        gridHeight = Mathf.Floor(Camera.main.orthographicSize-1);
+        gridWidth  = Mathf.Floor(gridHeight * Camera.main.aspect-1);
+       
+        //spawn first food
+        foodClone = Instantiate(food, Vector3.zero, transform.rotation);
+
+        //allot food position
+        FoodRelocate();
+
+        //MakeGrid();
+
+        //call function in perticular time interval
+        InvokeRepeating("TailFollow", 0, instance.gameSpeed);
+    }
+
+
+
+    //make grid
+    void MakeGrid()
+    {
+        for(float i = -gridWidth; i <= gridWidth; i += instance.snakeMoveDist)
+        {
+            for (float j = -gridHeight;j <= gridHeight; j += instance.snakeMoveDist)
+            {
+                Instantiate(tailPart, new Vector3(i, j), transform.rotation);
+            }
+        }
+    }
+	
+	void Update () {
+        RepositionTail();
+	}
+
+    //repositioning tails
+    void RepositionTail()
+    {
+        for (int i = 0; i < tailPos.Count; i++)
+        {
+            tail[i].transform.position = tailPos[i];
+        }
+    }
+
+    //change food position after food eaten
+	public void FoodRelocate(){
+
+        x = Mathf.Floor(Random.Range(-gridWidth, gridWidth)) * instance.snakeMoveDist;
+        y = Mathf.Floor(Random.Range(-gridHeight, gridHeight)) * instance.snakeMoveDist;
+
+        foreach(Vector2 pos in tailPos)
+        {
+            if(pos == new Vector2(x, y))
+            {
+                FoodRelocate();
+                return;
+            }
+        }
+        foodClone.transform.position = new Vector2(x, y);
+    }
+
+    //save head position in a vector2 array
+    void TailFollow()
+    {
+        
+        CheckFoodCollision();
+        CheckBodyCollision();
+        tailPos.Add(instance.transform.position);
+        if (tailPos.Count > totalEat)
+        {
+            tailPos.RemoveAt(0);
+        }
+    }
+
+    //check food eaten
+    void CheckFoodCollision()
+    {
+        float dis = Mathf.Abs(Vector2.Distance(foodClone.transform.position, instance.transform.position));
+        if (dis < 1)
+        {
+            totalEat++;
+            FoodRelocate();
+            tail.Add(Instantiate(tailPart , instance.transform.position,transform.rotation));
+        }
+    }
+
+    //check head collide with body
+    void CheckBodyCollision()
+    {
+        foreach (Vector3 pos in tailPos)
+        {
+            if (instance.transform.position == pos)
+            {
+                totalEat = 0;
+                foreach(GameObject t in tail)
+                {
+                    Destroy(t);
+
+                }
+                tail.Clear();
+                tailPos.Clear();
+                return;
+            }
+        }
+    }
+
+}
---
+++ SnakeGame/Assets/Script/SnakeMovement.cs
@@ -0,0 +1,95 @@
+﻿
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour {
+
+
+    public float snakeMoveDist;
+    public float gameSpeed;
+
+    float x, y;
+    float xDir = 1, yDir;    
+    float height, width;
+    bool allowChangeDir = true;
+
+
+    private void Start()
+    {
+        InvokeRepeating("Move",0,gameSpeed);
+
+        x = transform.position.x;
+        y = transform.position.y;
+
+        height = Camera.main.orthographicSize-.5f;
+        width = height * Camera.main.aspect;
+    }
+
+    private void Update()
+    {
+
+        transform.position = new Vector2(x, y);
+        Movements();
+        WallPass();
+    }
+
+    //move snake head
+    void Move()
+    {
+        x += snakeMoveDist * xDir;
+        y += snakeMoveDist * yDir;
+        allowChangeDir = true;
+    }
+
+    //head movement
+    void Movements()
+    {
+        if (!allowChangeDir) return;
+
+        if (Input.GetKey(KeyCode.UpArrow) && yDir!=-1)
+        {
+            allowChangeDir = false;
+            yDir = 1;
+            xDir = 0;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && yDir != 1)
+        {
+            allowChangeDir = false;
+            yDir = -1;
+            xDir = 0;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && xDir != -1)
+        {
+            allowChangeDir = false;
+            yDir = 0;
+            xDir = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && xDir != 1)
+        {
+            allowChangeDir = false;
+            yDir = 0;
+            xDir =-1;
+        }
+    }
+
+    //reposition snake after snake passes the camera position
+    void WallPass()
+    {
+        if (y > height)
+        {
+            y = -height;
+        }
+        else if (y < -height)
+        {
+            y = height;
+        }
+        else if (x < -width)
+        {
+            x=width;
+        }
+        else if (x > width)
+        {
+            x = -width;
+        }
+
+    }
+}
